79ec8181fd5f6a1ec78acb11c820715d7099ae45
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-12-17 21:30:24 +0100

    update basics questions

--- src/data/questions/basics.js
+++ src/data/questions/basics.js
@@ -1,9 +1,9 @@
 export const basicQuestions = [
     {
         id: 1,
-        question: "Qu'est-ce qu'un composant React?",
+        question: "Qu'est-ce qu'un composant React ?",
         options: [
-            "Une fonction JavaScript qui retourne du HTML",
+            "Une fonction JavaScript qui retourne du JSX",
             "Une classe CSS",
             "Un fichier HTML",
             "Une base de données"
@@ -13,7 +13,7 @@ export const basicQuestions = [
     },
     {
         id: 2,
-        question: "Que sont les props dans React?",
+        question: "Que sont les props dans React ?",
         options: [
             "Des propriétés CSS",
             "Des arguments passés aux composants",
@@ -25,7 +25,7 @@ export const basicQuestions = [
     },
     {
         id: 3,
-        question: "À quoi sert le hook useState?",
+        question: "À quoi sert le hook useState ?",
         options: [
             "Pour créer des routes",
             "Pour styler des composants",
@@ -37,7 +37,7 @@ export const basicQuestions = [
     },
     {
         id: 4,
-        question: "Comment créer un composant React?",
+        question: "Comment créer un composant React ?",
         options: [
             "En créant une fonction JavaScript qui retourne du JSX",
             "En créant une classe avec extends React.Component",
@@ -49,7 +49,7 @@ export const basicQuestions = [
     },
     {
         id: 5,
-        question: "Quel est le rôle de JSX?",
+        question: "Quel est le rôle de JSX ?",
         options: [
             "Faire du rendu côté serveur",
             "Écrire du code HTML dans du JavaScript",
@@ -61,31 +61,31 @@ export const basicQuestions = [
     },
     {
         id: 6,
-        question: "Quel hook permet de créer un effet secondaire dans React?",
+        question: "Quel hook permet de créer un effet secondaire dans React ?",
         options: [
             "useState",
-            "useEffect",
+            "useReducer",
             "useContext",
-            "useReducer"
+            "useEffect"
         ],
-        correctAnswer: 1,
+        correctAnswer: 3,
         category: "basics"
     },
     {
         id: 7,
-        question: "Comment peut-on passer des données d'un composant parent à un composant enfant?",
+        question: "Comment peut-on passer des données d'un composant parent à un composant enfant ?",
         options: [
-            "Avec les props",
-            "Avec les states",
             "Avec useState",
+            "Avec les states",
+            "Avec les props",
             "Avec useEffect"
         ],
-        correctAnswer: 0,
+        correctAnswer: 2,
         category: "basics"
     },
     {
         id: 8,
-        question: "Quels types de composants peut-on créer en React?",
+        question: "Quels types de composants peut-on créer en React ?",
         options: [
             "Composants fonctionnels",
             "Composants de classe",
@@ -97,7 +97,7 @@ export const basicQuestions = [
     },
     {
         id: 9,
-        question: "Que fait la méthode render() dans un composant React de classe?",
+        question: "Que fait la méthode render() dans un composant React de classe ?",
         options: [
             "Elle retourne le JSX à afficher",
             "Elle est utilisée pour les événements",
@@ -109,7 +109,7 @@ export const basicQuestions = [
     },
     {
         id: 10,
-        question: "Qu'est-ce que le Virtual DOM?",
+        question: "Qu'est-ce que le Virtual DOM ?",
         options: [
             "C'est une version réelle du DOM",
             "C'est une abstraction du DOM réel qui permet d'optimiser les mises à jour",
@@ -121,7 +121,7 @@ export const basicQuestions = [
     },
     {
         id: 11,
-        question: "À quoi sert la clé (key) dans une liste d'éléments React?",
+        question: "À quoi sert la clé (key) dans une liste d'éléments React ?",
         options: [
             "Pour identifier chaque élément de manière unique lors du rendu",
             "Pour éviter les erreurs de syntaxe",
@@ -133,7 +133,7 @@ export const basicQuestions = [
     },
     {
         id: 12,
-        question: "Quel est le rôle de ReactDOM?",
+        question: "Quel est le rôle de ReactDOM ?",
         options: [
             "Gérer les états de React",
             "Mettre à jour le DOM en fonction des changements d'état",
@@ -145,7 +145,7 @@ export const basicQuestions = [
     },
     {
         id: 13,
-        question: "Comment déclare-t-on un composant fonctionnel en React?",
+        question: "Comment déclare-t-on un composant fonctionnel en React ?",
         options: [
             "function MyComponent() {}",
             "const MyComponent = () => {}",
@@ -157,7 +157,7 @@ export const basicQuestions = [
     },
     {
         id: 14,
-        question: "Comment faire un rendu conditionnel dans JSX?",
+        question: "Comment faire un rendu conditionnel dans JSX ?",
         options: [
             "Avec un if-else",
             "Avec un opérateur ternaire",
@@ -169,19 +169,19 @@ export const basicQuestions = [
     },
     {
         id: 15,
-        question: "Qu'est-ce qu'un hook dans React?",
+        question: "Qu'est-ce qu'un hook dans React ?",
         options: [
             "Une fonction qui permet de manipuler des objets",
-            "Une fonctionnalité qui permet de gérer des états et effets dans les composants fonctionnels",
             "Un objet qui contient des props",
+            "Une fonctionnalité qui permet de gérer des états et effets dans les composants fonctionnels",
             "Un élément du DOM"
         ],
-        correctAnswer: 1,
+        correctAnswer: 2,
         category: "basics"
     },
     {
         id: 16,
-        question: "Qu'est-ce qu'une prop de type children?",
+        question: "Qu'est-ce qu'une prop de type children ?",
         options: [
             "Une prop qui permet de passer des données",
             "Une prop qui permet de passer un composant à un autre",
@@ -193,50 +193,50 @@ export const basicQuestions = [
     },
     {
         id: 17,
-        question: "Dans un composant de classe, où stocke-t-on l'état?",
+        question: "Dans un composant de classe, où stocke-t-on l'état ?",
         options: [
             "Dans une variable normale",
-            "Dans l'objet this.state",
             "Dans un hook useState",
+            "Dans l'objet this.state",
             "Aucune des réponses"
         ],
-        correctAnswer: 1,
+        correctAnswer: 2,
         category: "basics"
     },
     {
         id: 18,
-        question: "Comment peut-on envoyer un événement dans React?",
+        question: "Comment peut-on envoyer un événement dans React ?",
         options: [
             "Avec une fonction de rappel",
-            "Avec un gestionnaire d'événements",
             "Avec useState",
-            "Avec useEffect"
+            "Avec useEffect",
+            "Avec un gestionnaire d'événements"
         ],
-        correctAnswer: 1,
+        correctAnswer: 3,
         category: "basics"
     },
     {
         id: 19,
-        question: "Qu'est-ce qu'un formulaire contrôlé en React?",
+        question: "Qu'est-ce qu'un formulaire contrôlé en React ?",
         options: [
-            "Un formulaire dont les valeurs sont gérées par React",
             "Un formulaire dont les valeurs sont stockées dans une base de données",
             "Un formulaire dont les valeurs sont définies par JavaScript",
+            "Un formulaire dont les valeurs sont gérées par React",
             "Un formulaire sans champ de saisie"
         ],
-        correctAnswer: 0,
+        correctAnswer: 2,
         category: "basics"
     },
     {
         id: 20,
-        question: "Que signifie 'Unidirectionnal Data Flow' en React?",
+        question: "Que signifie 'Unidirectionnal Data Flow' en React ?",
         options: [
-            "Les données ne peuvent circuler que dans un seul sens, du parent à l'enfant",
             "Les données circulent dans tous les sens",
             "Les données sont envoyées depuis la base de données",
-            "Les données sont envoyées depuis l'enfant vers le parent"
+            "Les données sont envoyées depuis l'enfant vers le parent",
+            "Les données ne peuvent circuler que dans un seul sens, du parent à l'enfant"
         ],
-        correctAnswer: 0,
+        correctAnswer: 3,
         category: "basics"
     }
 ];
